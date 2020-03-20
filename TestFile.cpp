a1c118e0e5f4bd18a9d20ca656e41da2cd52cf04
Shin Jeong Min <sin37minduke@naver.com>
2020-03-21 01:45:15 +0900

    solve 4948

---
+++ TestFile.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//vector<int> vec;
+//
+//int main() {
+//	vec.assign(3, 0);
+//
+//	vector<int>::iterator iter = vec.begin();
+//
+//	for (int i = 0; i < vec.size(); i++) {
+//		cout << vec[i] << endl;
+//	}
+//}
