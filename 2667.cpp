3deded0e4084a3647580bbb20f61b9b8203f5e60
Shin Jeong Min <sin37minduke@naver.com>
2020-06-02 04:15:13 +0900

    solve 11123, 1012

---
+++ 2667.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <utility>
+#include <set>
+#include <unordered_set>
+using namespace std;
+
+int main()
+{
+
+}
+/*
+*/
--- TestFile.cpp
+++ TestFile.cpp
@@ -10,46 +10,12 @@
 //#include <unordered_set>
 //using namespace std;
 //
-//bool check[1000 + 1]; //idx 1부터 1000까지이므로 + 1
-//vector<vector<int>> v;
-//queue<int> q;
+//int check[10][10] = { 0, };
 //
 //int main() {
 //	ios_base::sync_with_stdio;
 //	cin.tie(0);
 //    
-//    int size;
-//    cin >> size;
-//
-//    vector<int> tempvec;
-//    v.assign(size + 1, tempvec);
-//
-//	int n, m;
-//	while (cin >> n >> m)
-//	{
-//		v[n].push_back(m);
-//		v[m].push_back(n); // 반대방향
-//	}
-//
-//    q.push(1); // root 노트의 값이 1이다.
-//    check[1] = true;
-//    while (!q.empty())
-//    {
-//        n = q.front();
-//        if (check[n])
-//        {
-//            cout << q.front() << " ";
-//            q.pop();
-//            for (int i = 0; i < v[n].size(); i++)
-//            {
-//                if (!check[v[n][i]])
-//                {
-//                    q.push(v[n][i]); // 탐색을 하지 않은 노드면 스택에 집어넣어
-//                    check[v[n][i]] = true; // 탐색 체크
-//                }
-//            }
-//        }
-//    }
 //}
 ///*
 //9
