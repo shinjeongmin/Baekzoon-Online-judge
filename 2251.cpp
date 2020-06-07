9437f328ef6f498eeda11e7825fb3a1211380118
Shin Jeong Min <sin37minduke@naver.com>
2020-06-07 15:19:50 +0900

    solve 10026, 11403, 2583, unsolve 2251

---
+++ 2251.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <utility>
+#include <set>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio;
+	cin.tie(0);
+
+	int A, B, C;
+	cin >> A >> B >> C;
+
+
+}
+/*
+*/
